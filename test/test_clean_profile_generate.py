d4145361b804e2a8387d38e348cf19c2db0a40c3
Erik Soma <stillusingirc@gmail.com>
2021-03-12 21:23:07 -0500

    Add tests for clean_profile_generate when the lib and temp dirs do not exist.

--- test/test_clean_profile_generate.py
+++ test/test_clean_profile_generate.py
@@ -5,6 +5,7 @@ import pgo
 import pytest
 # python
 import os
+import shutil
 import sys
 # setuptools
 import distutils.errors
@@ -46,6 +47,7 @@ def test_default_dirs(argv, distribution):
     assert os.path.basename(cmd.build_lib).startswith('.pgo-')
     assert os.path.basename(cmd.build_temp).startswith('.pgo-')
     
+
 def test_set_dirs(argv, distribution):
     argv.extend([
         'clean_profile_generate',
@@ -84,6 +86,36 @@ def test_run(argv, distribution, pgo_lib_dir, pgo_temp_dir):
     distribution.parse_command_line()
     assert os.path.isdir(pgo_lib_dir)
     assert os.path.isdir(pgo_temp_dir)
+    distribution.run_commands()
+    assert not os.path.isdir(pgo_lib_dir)
+    assert not os.path.isdir(pgo_temp_dir)
+    
+    
+def test_run_lib_does_not_exist(argv, distribution, pgo_lib_dir, pgo_temp_dir):
+    argv.extend([
+        'clean_profile_generate',
+        '--build-lib', pgo_lib_dir,
+        '--build-temp', pgo_temp_dir,
+    ])
+    distribution.parse_command_line()
+    assert os.path.isdir(pgo_lib_dir)
+    assert os.path.isdir(pgo_temp_dir)
+    shutil.rmtree(pgo_lib_dir)
+    distribution.run_commands()
+    assert not os.path.isdir(pgo_lib_dir)
+    assert not os.path.isdir(pgo_temp_dir)
+    
+    
+def test_run_temp_does_not_exist(argv, distribution, pgo_lib_dir, pgo_temp_dir):
+    argv.extend([
+        'clean_profile_generate',
+        '--build-lib', pgo_lib_dir,
+        '--build-temp', pgo_temp_dir,
+    ])
+    distribution.parse_command_line()
+    assert os.path.isdir(pgo_lib_dir)
+    assert os.path.isdir(pgo_temp_dir)
+    shutil.rmtree(pgo_temp_dir)
     distribution.run_commands()
     assert not os.path.isdir(pgo_lib_dir)
     assert not os.path.isdir(pgo_temp_dir)
